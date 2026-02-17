666fb0b48952311dec70d621ab0b4d7ca3dc5020
NickOsipov <nick.osipov.91@gmail.com>
2026-02-17 22:02:43 +0300

    feat(ci): add deployment job to CI/CD pipeline with entrypoint script execution

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+source .bashrc
+
 export STREMLIT_PID=$(lsof -ti:8501) || true
 kill $STREMLIT_PID || true
 
+source .venv/bin/activate
 python3 src/train.py
 nohup streamlit run src/app.py --server.port 8501 >> streamlit.log 2>&1 &
